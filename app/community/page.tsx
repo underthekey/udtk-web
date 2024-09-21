c0dc0fe6c746ed2e49d8a9d9b1805415ac5d058c
takealook97 <takealook97@naver.com>
2024-09-22 03:39:19 +0900

    refactor: move header to button and set typer to main page

--- app/community/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '@/styles/PageDefault.module.css';
-
-export default function CommunityPage() {
-  return (
-    <div className={styles.pageContainer}>
-      
-    </div>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import styles from '@/styles/Home.module.css';
+import Typer from '@/components/Typer';
+import { Sentence } from './types';
 
+async function getSentences() {
+  const res = await fetch('https://sentence.udtk.site/random?count=20', { cache: 'no-store' });
+  if (!res.ok) {
+    throw new Error('Failed to fetch sentences');
+  }
+  return res.json();
+}
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Welcome to underthekey.</h1>
-        <p>Explore all about custom keyboards.</p>
-      </main>
-    </div>
-  );
+export default async function Home() {
+  const initialSentences: Sentence[] = await getSentences();
+
+  return <Typer initialSentences={initialSentences} />;
 }
